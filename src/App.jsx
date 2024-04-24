5c97be632bd956899c283ff2bd4c0adcb0d99b5f
FasihAhmedRaza <thisfasih@gmail.com>
2024-04-24 13:02:21 +0500

    nav start

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css';
+import FloatingNav from './components/Nav';
+function App() {
+  return (
+    <div>
+    {/* <h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1> */}
+  <FloatingNav/>
+  
+  </div>
+  );
+}
+
+export default App;
